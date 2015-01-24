310db8f66a1dfc1d48690ea2eb1b1780f42672f0
Sankha Narayan Guria <sankha93@gmail.com>
2015-01-24 17:21:46 +0530

    timer working

--- bhaag/pages/hub/section2Page.js
+++ bhaag/pages/hub/section2Page.js
@@ -17,4 +17,54 @@
     WinJS.Namespace.define("HubApps_SectionControls", {
         Section2Control: ControlConstructor
     });
+
 })();
+
+function checkTime(i) {
+    if (i < 10)
+        return "0" + i;
+    else
+        return i;
+}
+
+var running = false;
+var runningTick;
+
+function beginRun() {
+    var timer = document.getElementById("timer");
+    var runBtn = document.getElementById("runBtn");
+    runBtn.innerHTML = "Stop Running";
+    var h = 0, m = 0, s = 0;
+    var updateTime = function () {
+        s++;
+        if (s > 59) {
+            m++;
+            s = 0;
+        }
+        if (m > 59) {
+            h++;
+            m = 0;
+        }
+        var s_h = checkTime(h),
+            s_m = checkTime(m),
+            s_s = checkTime(s);
+        timer.innerHTML = s_h + ":" + s_m + ":" + s_s;
+    };
+    running = true;
+    runningTick = setInterval(updateTime, 1000);
+}
+
+function endRun() {
+    var timer = document.getElementById("timer");
+    var runBtn = document.getElementById("runBtn");
+    runBtn.innerHTML = "Start Running";
+    clearInterval(runningTick);
+    running = false;
+}
+
+function startRunTimer(e) {
+    if (!running)
+        beginRun();
+    else
+        endRun();
+}
