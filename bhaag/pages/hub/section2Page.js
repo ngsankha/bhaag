f068521da0284bc3ad6d4715b8a2c7e1c5097ff1
Sankha Narayan Guria <sankha93@gmail.com>
2015-01-25 07:25:35 +0530

    stride measurement

--- bhaag/pages/hub/section2Page.js
+++ bhaag/pages/hub/section2Page.js
@@ -24,8 +24,9 @@
 
 var running = false;
 var runDist = 0;
-var runningTick, geo, geoWatch, lat, long;
-var lats = [], longs = [];
+var h, m, s;
+var runningTick, geo, geoWatch, lat, long, accelerometer;
+var lats, longs, times, speeds, acX, acY, acZ;
 
 function checkTime(i) {
     if (i < 10)
@@ -42,13 +43,14 @@ function showMessage(msg) {
 function initGeo() {
     geo = new Windows.Devices.Geolocation.Geolocator();
     geo.movementThreshold = 2;
+    geo.desiredAccuracy = Windows.Devices.Geolocation.PositionAccuracy.high;
     if (!geo)
         showMessage("Access to your location is turned off. Change your settings to turn it back on.");
 }
 
 function startTimer() {
     var timer = document.getElementById("timer");
-    var h = 0, m = 0, s = 0;
+    h = 0, m = 0, s = 0;
     var updateTime = function () {
         s++;
         if (s > 59) {
@@ -94,22 +96,26 @@ function distance(latitude1, longitude1, latitude2, longitude2) {
 function posChanged(args) {
     var pos = args.position;
     var distCounter = document.getElementById("distCounter");
+    var speedometer = document.getElementById("speedometer")
     if (lat == null) {
         lat = pos.coordinate.point.position.latitude;
         long = pos.coordinate.point.position.longitude;
-        lats.push(lat);
-        longs.push(long);
     } else {
         var lat2 = pos.coordinate.point.position.latitude,
-            long2 = pos.coordinate.point.position.longitude;
+            long2 = pos.coordinate.point.position.longitude,
+            speed = pos.coordinate.speed;
         lats.push(lat2);
         longs.push(long2);
+        speeds.push(speed);
+        times.push(Date.now());
         var dist = Math.abs(distance(lat2, long2, lat, long));
         lat = lat2;
         long = long2;
         runDist += dist;
         var tmp = Math.round(runDist * 100);
         distCounter.innerHTML = (tmp / 100) + " km";
+        tmp = Math.round(speed * 100);
+        speedometer.innerHTML = (tmp / 100) + " m/s";
     }
 }
 
@@ -118,17 +124,66 @@ function startDistanceTick() {
     geo.addEventListener("positionchanged", posChanged);
 }
 
+function accReadChange(e) {
+    if (running) {
+        var reading = e.reading;
+        var accelX = reading.accelerationX;
+        var accelY = reading.accelerationY;
+        var accelZ = reading.accelerationZ;
+        acX.push(accelX);
+        acY.push(accelY);
+        acZ.push(accelZ);
+    }
+}
+function initAccelerometer() {
+    if (accelerometer == null) {
+        accelerometer = Windows.Devices.Sensors.Accelerometer.getDefault();
+
+        // Establish the report interval
+        var minimumReportInterval = accelerometer.minimumReportInterval;
+        var reportInterval = minimumReportInterval > 16 ? minimumReportInterval : 16;
+        accelerometer.reportInterval = reportInterval;
+
+        // Establish the event handler
+        accelerometer.addEventListener("readingchanged", accReadChange);
+    }
+}
+
 function beginRun() {
+    lats = [], longs = [], times = [], speeds = [],
+    acX = [], acY = [], acZ = [];
     var timer = document.getElementById("timer");
     timer.innerHTML = "00:00:00";
     var distCounter = document.getElementById("distCounter");
     distCounter.innerHTML = "0.0 km";
+    var speedometer = document.getElementById("speedometer");
+    speedometer.innerHTML = "0.0 m/s";
     var runBtn = document.getElementById("runBtn");
     runBtn.innerHTML = "Stop Running";
     running = true;
     initGeo();
+    initAccelerometer();
     startTimer();
     startDistanceTick();
+}
+
+function writeData() {
+    var storage = window.localStorage;
+    var idx = storage.runTimes ? Number(storage.runTimes) : 0;
+    var obj = {
+        lats: lats,
+        longs: longs,
+        times: times,
+        speeds: speeds,
+        acX: acX,
+        acY: acY,
+        acZ: acZ,
+        distance: runDist,
+        time: 3600 * h + 60 * m + s
+    };
+    storage.setItem("run" + idx, JSON.stringify(obj));
+    storage.setItem("runTimes", idx + 1);
+    return idx;
 }
 
 function endRun() {
@@ -137,7 +192,12 @@ function endRun() {
     runBtn.innerHTML = "Start Running";
     clearInterval(runningTick);
     geo.removeEventListener("positionchanged", posChanged);
+    accelerometer = null;
+    var idx = writeData();
+    lats = [], longs = [], times = [], speeds = [],
+    acX = [], acY = [], acZ = [];
     running = false;
+    WinJS.Navigation.navigate("/pages/hub/summary.html", { item: idx });
 }
 
 function startRunTimer(e) {
@@ -146,3 +206,6 @@ function startRunTimer(e) {
     else
         endRun();
 }
+
+// DO NOT ENABLE THIS
+window.localStorage.clear();
---
+++ bhaag/pages/hub/summary.js
@@ -0,0 +1,92 @@
+﻿(function () {
+    "use strict";
+
+    var obj;
+
+    function showTime() {
+        var time = obj.time;
+        var s = time % 60;
+        time = Math.floor(time / 60);
+        var m = time % 60;
+        time = Math.floor(time / 60);
+        var str = (time < 10 ? "0" + time : time) + ":" + (m < 10 ? "0" + m : m) + ":" + (s < 10 ? "0" + s : s);
+        document.getElementById("duration").innerHTML += "<br/>" + str;
+    }
+
+    function showDistance() {
+        var dist = obj.distance;
+        var str = (Math.round(dist * 100) / 100) + " km";
+        document.getElementById("distance").innerHTML += "<br/>" + str;
+    }
+
+    function showSpeeds() {
+        var speeds = obj.speeds;
+        var maxm = speeds[0];
+        for (var i = 1; i < speeds.length; i++) {
+            if (maxm < speeds[i])
+                maxm = speeds[i];
+        }
+        var avg = Math.round((obj.distance * 1000 / obj.time) * 100) / 100;
+        document.getElementById("maxSpeed").innerHTML += "<br/>" + maxm + " m/s";
+        document.getElementById("avgSpeed").innerHTML += "<br/>" + avg + " m/s";
+    }
+
+    /*function computeDirns(arr) {
+        var ddx = [];
+        var totalddx = 0.0;
+        for (var i = 1; i < arr.length - 1; i++) {
+            ddx.push(arr[i + 1] + arr[i - 1] - 2 * arr[i]);
+            //ddx.push(arr[i + 1] - arr[i]);
+            totalddx += ddx[ddx.length - 1];
+        }  
+        var avgddx1 = totalddx / ddx.length,
+            avgddx2 = -avgddx1;
+        var numPeaks = 0;
+        var posAvg, negAvg;
+        if (avgddx1 > 0) {
+            posAvg = avgddx1;
+            negAvg = avgddx2;
+        } else {
+            negAvg = avgddx1;
+            posAvg = avgddx2;
+        }
+        for (var i = 0; i < ddx.length ; i++) {
+            if (ddx[i] > posAvg || ddx[i] < negAvg && Math.abs(arr[i + 1]) > 0.1)
+                numPeaks++;
+        }
+        return numPeaks;
+    }
+
+    function showStrides() {
+        var px = computeDirns(obj.acX);
+        var py = computeDirns(obj.acY);
+        var pz = computeDirns(obj.acZ);
+        //var maxp = Math.floor(Math.max(px, py, pz) / 2);
+        document.getElementById("strides").innerHTML += "<br/>" + px + "," + py + "," + pz;
+    }*/
+
+    function showStrides() {
+        var strides = 0;
+        var length;
+        for (var i = 0; i < obj.acX.length; i++) {
+            length = Math.sqrt(obj.acX[i] * obj.acX[i] + obj.acY[i] * obj.acY[i] + obj.acZ[i] * obj.acZ[i]);
+            if (length >= 1.5)
+                strides++;
+        }
+        if (strides > obj.time * 3)
+            strides = Math.round(Math.sqrt(strides));
+        document.getElementById("strides").innerHTML += "<br/>" + strides;
+    }
+
+    WinJS.UI.Pages.define("/pages/hub/summary.html", {
+        ready: function (element, options) {
+            var idx = options.item;
+            element.querySelector(".titlearea .pagetitle").textContent = "Summary";
+            obj = JSON.parse(window.localStorage.getItem("run" + idx));
+            showTime();
+            showDistance();
+            showSpeeds();
+            showStrides();
+        }
+    });
+})();
